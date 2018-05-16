b39b4f0f07f0aa3f2cee97633ae070873362768d
Derek Fields <DJFen101@gmail.com>
2018-05-16 15:40:50 -0400

    Delete movin.js, Change some modal things

--- js/moveIn.js
+++
@@ -1,6 +0,0 @@
-const moveIn = document.getElementById(('resume'||'projects') || ('hobbies'||'following'));
-		window.onload = function(){
-			if (moveIn.scrollIntoView() = true){
-				moveIn.classList.add('inLeft inRight');
-			}
-		}
