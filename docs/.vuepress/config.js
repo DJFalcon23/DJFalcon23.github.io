45b3c7a729ea98520f3cd515d767c2b4147606ba
Derek Fields <DJFen101@gmail.com>
2018-12-06 15:20:53 -0500

    change DJFal... to docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    title: 'DerekFields.is/...',
+	description:'Many cool things. Welcome to my site.',
+    themeConfig: {
+        nav: [
+            { text: "Home", link: '/' },
+            { text: 'Blogging', link: '/blogging/' },
+			{ text: 'Freelancing', link: '/freelancing/'},
+			{ text: 'Followed ',items: [
+          		{ text: 'Twitter', link: 'twitter.com/Djfalcon23'},
+				{ text: 'Github', link: 'github.com/djfalcon23' }
+        		]}
+        	],
+        
+    }
+}
