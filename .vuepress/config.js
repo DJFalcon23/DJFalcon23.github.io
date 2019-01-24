d34607a5b86ed13628aac078402377cc08fd792b
Derek Fields <DJFen101@gmail.com>
2019-01-24 18:47:18 -0500

    delete some generated files, add vue-deploy files

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,20 @@
 module.exports = {
     title: 'DerekFields.is/...',
 	description:'Many cool things. Welcome to my site.',
+	head: [
+		['link', { rel: 'icon', href: `/logo.png` }],
+		['link', { rel: 'manifest', href: '/manifest.json' }],
+		['link', { rel: 'canonical', href: 'https://derekfields.is' }],
+		['meta', { name: 'theme-color', content: '#3eaf7c' }],
+		['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+		['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+		['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
+		['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+		['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+		['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+		['script', { type:"text/javascript", src="https://identity.netlify.com/v1/netlify-identity-widget.js"}]
+	],
+	serviceWorker: true,
     themeConfig: {
         nav: [
             { text: "Home", link: '/' },
