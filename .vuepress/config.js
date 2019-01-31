2a785dc1cf6d5befc27d1daa06bf869ee4819ed3
Derek Fields <DJFen101@gmail.com>
2019-01-31 01:10:49 -0500

    Change icons

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -10,7 +10,7 @@ module.exports = {
 		['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
 		['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
 		['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
-		['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+		['meta', { name: 'msapplication-TileImage', content: '/icons/mstile-144x144.png' }],
 		['meta', { name: 'msapplication-TileColor', content: '#000000' }],
 		['script', { type:"text/javascript", src: 'https://identity.netlify.com/v1/netlify-identity-widget.js'}]
 	],
