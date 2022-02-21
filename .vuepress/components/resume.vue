3c04649ae2e7ca0e73f57004b2fd05626780d767
Derek J. Fields Jr <4926412+DJFalcon23@users.noreply.github.com>
2022-02-21 18:55:02 -0500

    Update resume.vue

--- .vuepress/components/resume.vue
+++ .vuepress/components/resume.vue
@@ -1,3 +1,5 @@
+<!-- /.vuepress/components/resume.vue -->
+
 <template>
-  <iframe src="https://docs.google.com/gview?url=https://drive.google.com/file/d/1C_EWtgEzW0fg595DJ_CZhNqnJdkNLdvj/view?usp=sharing&embedded=true" style="width:600px; height:500px;" frameborder="0"></iframe>
+  <iframe s<!-- /.vuepress/components/BlogIndex.vue -->rc="https://docs.google.com/gview?url=https://drive.google.com/file/d/1C_EWtgEzW0fg595DJ_CZhNqnJdkNLdvj/view?usp=sharing&embedded=true" style="width:600px; height:500px;" frameborder="0"></iframe>
 </template>
