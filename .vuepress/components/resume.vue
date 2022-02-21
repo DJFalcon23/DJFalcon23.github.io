8b8fff89932f3d9d829f3be68ccc3d5596140e68
Derek J. Fields Jr <4926412+DJFalcon23@users.noreply.github.com>
2022-02-21 18:47:50 -0500

    Create resume.vue

---
+++ .vuepress/components/resume.vue
@@ -0,0 +1,3 @@
+<template>
+  <iframe src="https://docs.google.com/gview?url=https://drive.google.com/file/d/1C_EWtgEzW0fg595DJ_CZhNqnJdkNLdvj/view?usp=sharing&embedded=true" style="width:600px; height:500px;" frameborder="0"></iframe>
+</template>
